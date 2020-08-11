0379382bae823c5375d034eb2c6e0f3c8cacbe52
Jonas Toussaint <jonastoussaint@Jonass-MacBook-Pro.local>
2020-08-11 03:00:48 -0400

    Added Video Card Container info

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,133 @@
 import React from 'react';
 import "./RecommendedVideos.css";
+import VideoCard from './VideoCard'
+
 
 function RecommendedVideos() {
     return (
         <div className="recommendedVideos">
-            <h2>This is where videos go.</h2>
+            <h2>Recommended</h2>
+            <div className="recommendedVideos__videos">
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+                
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+
+                <VideoCard 
+                title ="Become a Web Developer in 10 minutes | 2019/2020"
+                views = "2.3M"
+                timestamp="3 days ago"
+                channelImage="https://create-react-app.dev/img/logo-og.png"
+                channel="Sonny Sangha"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSuwGdBM3Yjo3qVgSwl6ffeyCpTSkzlj3QenQ&usqp=CAU"/>
+            </div>
         </div>
     )
 }
